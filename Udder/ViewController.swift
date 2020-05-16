4e494a8fbbdfd38961f1a5a48d881df5489ad49e
Maggie Guan <maggieguan149@gmail.com>
2020-05-15 21:51:21 -0700

    Initial Commit

---
+++ Udder/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Udder
+//
+//  Created by Maggie Guan on 5/15/20.
+//  Copyright © 2020 Maggie Guan. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
